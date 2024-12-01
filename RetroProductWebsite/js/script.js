099f965db34b67e6b9ef5af8726684cca33b5994
berkebaba31 <berkcark4@gmail.com>
2024-12-01 20:46:23 +0300

    Add files via upload

---
+++ RetroProductWebsite/js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('global-header').innerHTML = `
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="shop.html">Shop</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+`;
+
+document.getElementById('global-footer').innerHTML = `
+    <p>&copy; 2024 Sony Walkman Store | All Rights Reserved</p>
+`;
